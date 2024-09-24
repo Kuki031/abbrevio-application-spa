bbe8ad8c89b99abf58afa4fdabd4a503862a6585
Kuki031 <luka.kukucka1@gmail.com>
2024-09-24 20:51:45 +0200

    Connect to login api, add login functionality

--- src/app/auth-actions/login/login.component.ts
+++ src/app/auth-actions/login/login.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Injectable, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -9,24 +9,33 @@ import { merge } from 'rxjs';
 import { MatCardModule } from '@angular/material/card';
 import { AuthActionsService } from '../auth-service/auth-actions.service';
 import { CommonModule } from '@angular/common';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, MatButtonModule, MatCardModule, CommonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, MatButtonModule, MatCardModule, CommonModule, MatProgressSpinnerModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
+
 export class LoginComponent {
   readonly username = new FormControl('', [Validators.required]);
   readonly password = new FormControl('', [Validators.required]);
 
+  private _snackBar = inject(MatSnackBar);
+  isClicked: boolean = false;
+
   errorMessage = signal('');
+  errorMessages: string[] = [];
 
   hide = signal(true);
 
-  constructor(private authService: AuthActionsService) {
+  constructor(private authService: AuthActionsService, private cdRef: ChangeDetectorRef) {
     merge(this.username.statusChanges, this.username.valueChanges, this.password.statusChanges, this.password.valueChanges)
       .pipe(takeUntilDestroyed())
       .subscribe(() => this.updateErrorMessage());
@@ -44,25 +53,44 @@ export class LoginComponent {
     }
   }
 
-  login(): void {
-    const usernameValue = this.username.value || '';
-    const passwordValue = this.password.value || '';
+  login(e: MouseEvent): void {
+    const buttonEl = e.target as HTMLButtonElement;
+    const usernameValue = this.username?.value || '';
+    const passwordValue = this.password?.value || '';
 
-    if (!this.username.valid || !this.password.valid) {
-      console.error('Form is invalid');
-      return;
-    }
+    this.isClicked = true;
+    this.cdRef.detectChanges();
 
     this.authService.login(usernameValue, passwordValue).subscribe(
       (user) => {
         localStorage.setItem(user.tokenType, user.accessToken);
-        location.assign("home");
+
+        if (user) {
+          setTimeout(() => {
+            location.assign('home');
+            this.isClicked = false;
+            this.cdRef.detectChanges();
+          }, 2000);
+        }
       },
-      (error) => console.error('Login failed', error)
+      (error) => {
+        this.isClicked = false;
+        this.cdRef.detectChanges();
+        error.error.messages.forEach((m: string) => this.errorMessages.push(m));
+        this.errorMessages.forEach((m: string) => this.openSnackBar(m, 'close'));
+      }
     );
   }
 
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 2000,
+      verticalPosition: "top"
+    });
+  }
+
   clickEvent(event: MouseEvent) {
+    event.preventDefault();
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthActionsService } from '../../auth-actions/auth-service/auth-actions.service';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +13,16 @@ import { CommonModule } from '@angular/common';
 })
 export class HeaderComponent implements OnInit {
 
+  constructor(private authService: AuthActionsService) { }
+
   isLoggedIn: boolean = false;
   ngOnInit(): void {
     this.isLoggedIn = !!localStorage.getItem("Bearer");
+  }
 
+  logOut(): void {
+    this.authService.logout();
+    location.assign("/home");
   }
 
 }
