cee36dcb9ee51a150eed8bacc056daa91f5a7132
Kuki031 <luka.kukucka1@gmail.com>
2024-09-30 21:36:43 +0200

    Add footer component, update profile component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header/header.component";
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { FooterComponent } from './footer/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MatCardModule, MatButtonModule],
+  imports: [RouterOutlet, HeaderComponent, MatCardModule, MatButtonModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
